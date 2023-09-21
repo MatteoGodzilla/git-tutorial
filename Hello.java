e2fe80c5406c291f5c9eb0eca414318a036632ac
MatteoGodzilla <catena.matteo90@gmail.com>
2023-09-21 19:52:15 +0200

    Modifiche su Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 class Hello{
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Ciao Mondo");
+        System.out.println("Ciao Mondo");
+        System.out.println("Ciao Mondo");
+        System.out.println("Ciao Mondo");
+        System.out.println("Ciao Mondo");
     }
 }
